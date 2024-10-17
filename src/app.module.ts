abeefe7c15c7aada783de42900bdc7e01f75e108
Teodoro <biel.teodorob@gmail.com>
2024-10-17 09:31:41 -0300

    feat: Add HttpModule and ConfigModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+    HttpModule,
+  ],
   controllers: [],
   providers: [],
 })
